9390c64a342f636c6072ff759a27119c8db0f391
Denis Barko <denisbarko@hotmail.com>
2025-09-10 00:45:08 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/eduapp/EduappApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.eduapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduappApplication.class, args);
+	}
+
+}
