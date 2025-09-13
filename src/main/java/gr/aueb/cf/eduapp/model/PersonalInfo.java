e82012b3d64c0c0d7aa6ce3fbd9ffd9b07c76b6c
Denis Barko <denisbarko@hotmail.com>
2025-09-13 22:39:23 +0300

    feat: made amka and identity number unique

--- src/main/java/gr/aueb/cf/eduapp/model/PersonalInfo.java
+++ src/main/java/gr/aueb/cf/eduapp/model/PersonalInfo.java
@@ -14,8 +14,13 @@ public class PersonalInfo extends AbstractEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String amka;
+
+    @Column(unique = true)
     private String identityNumber;
+
     private String placeOfBirth;
     private String municipalityOfRegistration;
 
