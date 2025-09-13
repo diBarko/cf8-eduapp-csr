c1c43e21f718400b33e0c42ea05b68dca9bf1d41
Denis Barko <denisbarko@hotmail.com>
2025-09-13 22:09:56 +0300

    feat: add teacher service interface

---
+++ src/main/java/gr/aueb/cf/eduapp/service/ITeacherService.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.eduapp.service;
+
+import gr.aueb.cf.eduapp.core.exceptions.AppObjectAlreadyExists;
+import gr.aueb.cf.eduapp.core.exceptions.AppObjectInvalidArgumentException;
+import gr.aueb.cf.eduapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.eduapp.dto.TeacherReadOnlyDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface ITeacherService {
+
+    TeacherReadOnlyDTO saveTeacher(TeacherInsertDTO teacherInsertDTO, MultipartFile amkaFile)
+            throws AppObjectAlreadyExists, AppObjectInvalidArgumentException, IOException;
+
+    Page<TeacherReadOnlyDTO> getPaginatedTeachers(int page, int size);
+
+    // TBA Filtering
+}
