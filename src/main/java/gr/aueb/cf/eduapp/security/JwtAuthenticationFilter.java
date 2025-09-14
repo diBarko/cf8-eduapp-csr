61aa82982e0a7a7814cb6ae6c1cd5da96f07f184
Denis Barko <denisbarko@hotmail.com>
2025-09-15 00:56:04 +0300

    feat: add jwt filter

---
+++ src/main/java/gr/aueb/cf/eduapp/security/JwtAuthenticationFilter.java
@@ -0,0 +1,75 @@
+package gr.aueb.cf.eduapp.security;
+
+
+import gr.aueb.cf.eduapp.authentication.JwtService;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String username;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7).trim();
+        try {
+            username = jwtService.extractSubject(jwt);
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                if (!jwtService.isTokenValid(jwt, userDetails)) {
+                    throw new BadCredentialsException("Invalid Token");
+                }
+
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        } catch (ExpiredJwtException e) {
+            // triggers AuthenticationEntryPoint 401 error
+            throw new AuthenticationCredentialsNotFoundException("Expired token", e);
+        } catch (JwtException | IllegalArgumentException e) {
+            // triggers AuthenticationEntryPoint 401 error
+            throw new BadCredentialsException("Invalid Token");
+        } catch (Exception e) {
+            // triggers AccessDenied 403
+            throw new AccessDeniedException("Token validation failed");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
