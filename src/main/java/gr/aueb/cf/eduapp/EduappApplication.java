9d75b4343d3b18fb47902cf0819a455a123db13b
Denis Barko <denisbarko@hotmail.com>
2025-09-17 18:02:41 +0300

    feat: add comments

--- src/main/java/gr/aueb/cf/eduapp/EduappApplication.java
+++ src/main/java/gr/aueb/cf/eduapp/EduappApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
-@EnableJpaAuditing
+@SpringBootApplication  // SpringBoot app Entry Point
+@EnableJpaAuditing      // Enable log [Event, WhoDoneIt, When-Timestamp]
 public class EduappApplication {
 
 	public static void main(String[] args) {
